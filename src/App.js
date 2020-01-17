da250f9c8c0899cc369b96521c28f11110fb6008
RajithaKumara <tmrkumara.15@cse.mrt.ac.lk>
2020-01-17 14:58:41 +0530

    Add material-ui

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles, createMuiTheme } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import Typography from '@material-ui/core/Typography';
+
+const theme = createMuiTheme({
+    typography: {
+        useNextVariants: true,
+    },
+});
+
+const styles = theme => ({
+    text: {
+        margin: theme.spacing.unit,
+    },
+});
 
 class App extends React.Component {
     render() {
+        const { classes } = this.props;
         return (
-            <div>
-                <p>Hello world!</p>
-            </div>
+            <React.Fragment>
+                <MuiThemeProvider theme={theme}>
+                    <CssBaseline />
+                    <Typography className={classes.text}>
+                        Hello world!
+                    </Typography>
+                </MuiThemeProvider>
+            </React.Fragment>
         );
     }
 }
 
-export default App;
+App.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(App);
